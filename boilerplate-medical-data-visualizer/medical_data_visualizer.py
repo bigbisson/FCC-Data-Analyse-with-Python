01bc894fd21891868353c6794d8bc4cbd41033ce
Indra Nurwibisono <11659118+bigbisson@users.noreply.github.com>
2020-11-05 10:25:52 +0700

    draw_cat_plot

--- boilerplate-medical-data-visualizer/medical_data_visualizer.py
+++ boilerplate-medical-data-visualizer/medical_data_visualizer.py
@@ -16,14 +16,14 @@ df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(df,id_vars='cardio', value_vars=['active','alco','cholesterol','gluc','overweight','smoke'])
 
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the collumns for the catplot to work correctly.
-    df_cat = None
+    #df_cat = None
 
     # Draw the catplot with 'sns.catplot()'
-
+    fig = sns.catplot(data=df_cat,kind='count',x='variable',hue='value',col='cardio')
 
 
     # Do not modify the next two lines
