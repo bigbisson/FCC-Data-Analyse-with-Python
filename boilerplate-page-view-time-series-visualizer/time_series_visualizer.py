4238ecac4fd62944be9a63b56673ebe31621acf6
Indra Nurwibisono <11659118+bigbisson@users.noreply.github.com>
2020-11-05 17:37:36 +0700

    Draw Plot Line and Bar

--- boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
+++ boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
@@ -5,36 +5,38 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv("fcc-forum-pageviews.csv").set_index('date')
+df = pd.read_csv("fcc-forum-pageviews.csv", index_col='date')
 
 # Clean data
-df = None
-
+df = df[(df['value'] >= df['value'].quantile(0.025)) 
+        & (df['value'] <= df['value'].quantile(0.975))]
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
-
+    fig = df.plot(kind='line',figsize=(10,3)).figure
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
 
+
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.groupby([df.index.year,df.index.month])['value'].mean().unstack()
 
     # Draw bar plot
-
-
-
-
-
+    fig = df_bar.plot(kind='bar',legend=True,figsize=(10,10)).figure
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    plt.legend(labels=['January','February','March','April','May','June','July','August','September','October','November','December'])
+    
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
@@ -44,10 +46,6 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
-
-
-
-
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
